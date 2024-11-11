3b80a38fd64bc9597ede95c2210279c97b3b402d
Norodix <peceli.bence@gmail.com>
2024-11-11 19:24:10 +0100

    Make sticky light cooler

--- src/Spells/StickyLight/sticky_light_ball.gd
+++ src/Spells/StickyLight/sticky_light_ball.gd
@@ -1,14 +1,17 @@
 extends CharacterBody3D
 
 var stuck = false
+var goalpoint = Vector3.ZERO
 
 func _physics_process(delta: float) -> void:
 	if stuck:
+		global_position = lerp(global_position, goalpoint, 0.02)
 		return
 	velocity.y += -10 * delta
 	var col = move_and_collide(velocity * delta)
 	if col:
 		stuck = true
+		goalpoint = col.get_normal() * 2 + col.get_position()
 
 
 func _on_timer_timeout() -> void:
