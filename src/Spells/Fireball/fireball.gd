11d69afee1720e63b7d23c3a3353a92f6dbc0274
Norodix <peceli.bence@gmail.com>
2024-11-11 19:31:20 +0100

    Simplify fireball to characterbody

--- src/Spells/Fireball/fireball.gd
+++ src/Spells/Fireball/fireball.gd
@@ -1,26 +1,17 @@
-extends Node3D
+extends CharacterBody3D
 
-const speed = 10
+const speed = 30
 const lifetime_ms = 5000
 
 @onready var birthtime = Time.get_ticks_msec()
 @onready var explosion_scene : Resource = preload("res://Spells/Fireball/Explosion.tscn")
 
-func _process(delta: float) -> void:
-	self.global_transform.origin += speed * delta * self.global_transform.basis.z * -1
-	if birthtime + lifetime_ms < Time.get_ticks_msec():
-		self.queue_free()
 
 func _physics_process(delta: float) -> void:
-	var burnables = $BurnArea.get_overlapping_bodies()
-	for burnable in burnables:
-		if burnable.has_method("burn"):
-			burnable.burn()
-			self.queue_free()
-			spawn_explosion()
-			return
-	var bodies = $HitArea.get_overlapping_bodies()
-	if not bodies.is_empty():
+	if birthtime + lifetime_ms < Time.get_ticks_msec():
+		self.queue_free()
+	var col = move_and_collide(speed * delta * self.global_transform.basis.z * -1)
+	if col:
 		self.queue_free()
 		spawn_explosion()
 		return
