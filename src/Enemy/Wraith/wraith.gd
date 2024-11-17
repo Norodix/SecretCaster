5ec0fcc41add6ef349ce6fb126b1ab6307ca45ba
Norodix <peceli.bence@gmail.com>
2024-11-17 19:31:41 +0100

    Add basic melee enemy to game.
    No comment <3

---
+++ src/Enemy/Wraith/wraith.gd
@@ -0,0 +1,52 @@
+extends CharacterBody3D
+
+var target = Vector3.ZERO
+var speed = 5
+@onready var player = get_tree().root.find_child("Player", true, false)
+enum STRATEGY {
+	DIRECT,
+	FLEE,
+	SIDE_RIGHT,
+	SIDE_LEFT,
+	MAX, # should not be used and it must be the last enum
+}
+var strat = STRATEGY.DIRECT
+
+
+func burn():
+	destroy()
+
+
+func destroy():
+	self.queue_free()
+
+
+func _physics_process(delta: float) -> void:
+	var nextpos = $NavigationAgent3D.get_next_path_position()
+	velocity = (nextpos - global_position).normalized() * speed
+	if $NavigationAgent3D.is_navigation_finished():
+		velocity = Vector3.ZERO
+	#print(velocity)
+	move_and_slide()
+
+
+func _on_timer_timeout() -> void:
+	if not player:
+		var root = get_tree().root
+		player = root.find_child("Player", true, false)
+		return
+	# select new strategy
+	if randf() < 0.1:
+		strat = randi() % STRATEGY.MAX
+	var offset = global_position - player.global_position
+	match strat:
+		STRATEGY.DIRECT:
+			$NavigationAgent3D.target_position = player.global_position
+		STRATEGY.FLEE:
+			$NavigationAgent3D.target_position = player.global_position + offset.normalized() * 10
+		STRATEGY.SIDE_RIGHT:
+			var sidestep = offset.normalized().rotated(Vector3.UP, PI/2) * 10
+			$NavigationAgent3D.target_position = player.global_position + sidestep
+		STRATEGY.SIDE_LEFT:
+			var sidestep = offset.normalized().rotated(Vector3.UP, -PI/2) * 10
+			$NavigationAgent3D.target_position = player.global_position + sidestep
