9547c6c00d6ad9ada8bee042b0ce8f6ee4e968a8
bbluntblade <barna.bertalan95@gmail.com>
2024-11-18 16:42:17 +0100

    Fireball particle attractor

---
+++ src/Spells/Fireball/fireball_in_hand.gd
@@ -0,0 +1,9 @@
+extends Node3D
+
+var bufferPos = self.global_position
+@onready var attractor = $GPUParticlesAttractorSphere3D
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float) -> void:
+	attractor.global_position = self.global_position + ((self.global_position - bufferPos) * -1.0) + Vector3(0.0,0.1,0.0)
+	bufferPos = self.global_position
