6b27b4fd0f3d5bda66283bf5af0e713e4dd6f120
Norodix <peceli.bence@gmail.com>
2024-11-27 08:15:00 +0100

    Improve platform movement smoothness

--- src/Props/Platform/platform.gd
+++ src/Props/Platform/platform.gd
@@ -1,24 +1,19 @@
 extends Node3D
 
-@onready var player = get_tree().root.find_child("Player")
+@onready var player = get_tree().root.find_child("Player", true, false)
 const offset_max = 10
-const offset_min = 0
 const distance_max = 10
-const distance_min = 5
-var offset_target = 0
+var t : float # parametric easing function variable
+var t_target : float
 
 func _process(delta: float) -> void:
 	if not player:
 		player = get_tree().root.find_child("Player", true, false)
 		return
 	var d = global_position.distance_to(player.global_position)
-	if d < distance_max:
-		#visible = true
-		offset_target = offset_min
-	else:
-		#visible = false
-		offset_target = offset_max
-	var y = $Cylinder.transform.origin.y
-	y = lerp(y, float(- offset_target), 0.04)
+	t_target = 0 if d < distance_max else 1
+	t += sign(t_target - t) * 0.02
+	t = clamp(t, 0, 1)
+	var y = ease(t, -2) * offset_max * -1
 	visible = (y > - offset_max * 0.9)
 	$Cylinder.transform.origin.y = y
