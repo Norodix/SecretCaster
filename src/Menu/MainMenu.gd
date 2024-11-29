9ab711a1d181d1ef7e4ca737eb608202a15adf7a
Norodix <peceli.bence@gmail.com>
2024-11-29 15:19:16 +0100

    Add settings addon and create settings menu

--- src/Menu/MainMenu.gd
+++ src/Menu/MainMenu.gd
@@ -13,3 +13,13 @@ func _on_button_pressed() -> void:
 func _on_button_2_pressed() -> void:
 	get_tree().change_scene_to_file("res://Levels/Cuba/cuba.tscn")
 	pass # Replace with function body.
+
+
+func _on_button_4_pressed() -> void:
+	get_tree().quit()
+	pass # Replace with function body.
+
+
+func _on_button_3_pressed() -> void:
+	get_tree().change_scene_to_file("res://Menu/Settings.tscn")
+	pass # Replace with function body.
